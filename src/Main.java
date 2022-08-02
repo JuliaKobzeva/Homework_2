8b561dddc664d57524abdffcd10ad1375c065d77
Юля Кобзева <selemina@mail.ru>
2022-08-02 16:26:18 +0300

    ДЗ выполнено с правками из видео

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,44 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+
+    }
+    public static void task1() {
         byte aaa = 10;
         short bbb = 1_000;
         int ccc = 100_000;
         long ddd = 1_000_000_000_000_000L;
         float eee = 5.7f;
         double fff = 5.7777777;
+        boolean sentence = true;
+        char c = 'c';
         System.out.println(aaa);
         System.out.println(bbb);
         System.out.println(ccc);
         System.out.println(ddd);
         System.out.println(eee);
         System.out.println(fff);
+        System.out.println(sentence);
+        System.out.println(c);
+
+    }
+
+    public static void task2() {
 
         float Weight1 = 78.2f;
         float Weight2 = 82.7f;
         float Weight3 = Weight1 + Weight2;
-        float WeightDifference = Weight2 - Weight1;
+        float WeightDifference = Math.abs(Weight1 - Weight2);
         System.out.println("Общий вес боксеров " + Weight3 + " кг");
         System.out.println("Разница между весом бойцов " + WeightDifference + " кг");
+
+    }
+
+    public static void task3() {
 
         short bananaWeight = 80;
         short milkWeight = 105;
@@ -30,18 +50,26 @@ public class Main {
         short egg = 4;
         int breakfastWeight = (bananaWeight * banana) + (milkWeight * milk) + (icecreamWeight * icecream) + (eggWeight * egg);
         System.out.println(breakfastWeight);
-        float Kg = breakfastWeight/1000;
+        double Kg = breakfastWeight / (1000 * 1.0);
         System.out.println(Kg);
 
+    }
+
+    public static void task4() {
+
         int WeightLoss = 7000;
-        int Days1 = 7000/250;
-        int Days2 = 7000/500;
-        int Days3 = (Days1+Days2)/2;
+        int Days1 = WeightLoss / 250;
+        int Days2 = WeightLoss / 500;
+        double Days3 = (Days1 + Days2) / (2*1.0);
         int TimeDifference = Days2 % Days1;
         System.out.println("При потере веса 250 г в день спорсмен похудеет за " + Days1 + " дней");
         System.out.println("При потере веса 500 г в день спорсмен похудеет за " + Days2 + " дней");
         System.out.println("В среднем ему потребуется " + Days3 + " дней");
         System.out.println("Разница между диетами " + TimeDifference + " дней");
+
+    }
+
+    public static void task5() {
 
         int MashaSalary1 = 67_760;
         int DenisSalary1 = 83_690;
